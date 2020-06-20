dc23eac9427ff35184898540042b9a0f579f72f4
Cintia <Cintia@LAPTOP-EAES2S09>
2020-06-19 21:36:07 -0300

    Asunto: Inicialización del proyecto Responsable: Cintia Zamudio

---
+++ src/main/java/ar/edu/unju/fi/TrabajoFinalTesteoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalTesteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalTesteoApplication.class, args);
+	}
+
+}
